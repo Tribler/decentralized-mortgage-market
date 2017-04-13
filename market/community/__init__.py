5f50c6bf9f6821e05657279e0c1133fd13c3395c
Egbert Bouman <ebouman@gmail.com>
2017-04-13 16:09:05 +0200

    Import FieldDescriptor instead of using a hard-coded value

--- market/community/__init__.py
+++ market/community/__init__.py
@@ -1,4 +1,6 @@
 import protobuf_to_dict
 
-protobuf_to_dict.TYPE_CALLABLE_MAP[12] = str
-protobuf_to_dict.REVERSE_TYPE_CALLABLE_MAP.pop(12)
+from google.protobuf.descriptor import FieldDescriptor
+
+protobuf_to_dict.TYPE_CALLABLE_MAP[FieldDescriptor.TYPE_BYTES] = str
+protobuf_to_dict.REVERSE_TYPE_CALLABLE_MAP.pop(FieldDescriptor.TYPE_BYTES)
