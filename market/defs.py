ea4fe4ffbb8baf1181741fe827bcb705b61e3a6b
Egbert Bouman <ebouman@gmail.com>
2017-07-26 15:03:51 +0200

    Changed bank ids

--- market/defs.py
+++ market/defs.py
@@ -5,6 +5,6 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file_
 DEFAULT_REST_API_PORT = 8085
 DEFAULT_DISPERSY_PORT = 7795
 
-VERIFIED_BANKS = {'ABN-Amro': 'TGliTmFDTFBLOoc8OkIAtq9_8j5NmUG6tUWR473jLdUeyzqoHT0bVBoIb6bc4fVAV_9ejYro-XXqfl7yKmyblEoUH_AcgnEzzN0=',
-                  'Rabobank': 'TGliTmFDTFBLOrhXMbHGgk_Pq0TXYqMOCY7FTbtvwe3sXm9XbbzO0bkPsYfofTeAUUDtcBiPCEP2ntMQLdF-oeVr5FeXPoEbwpE=',
-                  'ING': 'TGliTmFDTFBLOiNVoxDL3S0wHOcEN7bdciOQzZfNYqyNUM9eQGmUNC5sRtKsb-KXzu8z1UJ-3ffNMzrtTXN-Vc9O8qmN3ymUKa4='}
+VERIFIED_BANKS = {'ABN-Amro': '1Db1195iPhqUtoHMna6Yh2pSvQ4=',
+                  'Rabobank': 'TFFLNVUP67e5kYLguUkNNm2GTm4=',
+                  'ING': 'XC5lM7C5cPl1nGTOBHIvv4ldaO0='}
